f728e9957919e94feff3aa3941734da6934b8b35
Stan <stan@digita8.co>
2025-03-13 00:51:39 +0200

    server

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const path = require('path');
+const basicAuth = require('express-basic-auth');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Set HTTP basic auth credentials
+app.use(basicAuth({
+    users: { 'eyeclinic': 'eyecl1n!c#' },
+    challenge: true,  // Shows browser popup for credentials
+}));
 
 // Serve static files from the React build
 app.use(express.static(path.join(__dirname, 'build')));
